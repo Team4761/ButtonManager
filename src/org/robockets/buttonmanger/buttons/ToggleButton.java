94846d81935d0e89b68f32f032c0ebcd9056036a
Jake Kinsella <jake.kinsella@gmail.com>
2015-10-27 19:55:45 -0400

    Update ToggleButton by actually cancelling the command to run alternate method

--- src/org/robockets/buttonmanger/buttons/ToggleButton.java
+++ src/org/robockets/buttonmanger/buttons/ToggleButton.java
@@ -7,24 +7,34 @@ import edu.wpi.first.wpilibj.command.Command;
 public class ToggleButton extends Button {
 	private boolean toggle = false;
 
+	/**
+	 * The command should have some code to run in the run method and other code to run in the interrupted method
+	 */
 	public ToggleButton(int joystickNumber, int buttonNumber, Command command) {
 		super(joystickNumber, buttonNumber, command);
 	}
 
 	// Only runs if button is not pushed
 	public void notPressed() {
-		if (running) {
-			stop();
-		}
+		running = false;
 	}
 	
 	public void pressed() {
 		if (!toggle && !running) {
+			toggle = true;
 			super.start();
+		} else if (toggle && !running) {
+			toggle = false;
+			super.stop();
 		}
 	}
 	
 	public void stop() {
 		super.stop();
+	}
+	
+	// True means running "run" and false means running "interrupted"
+	public boolean getToggleStatus() {
+		return toggle;
 	}
 }
