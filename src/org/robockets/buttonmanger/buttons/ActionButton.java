7f18470502a294c0dd39cd0e17f4da862c4a1187
Jake Kinsella <jake.kinsella@gmail.com>
2015-10-27 19:58:26 -0400

    Add option to run once or over and over again to Action button

--- src/org/robockets/buttonmanger/buttons/ActionButton.java
+++ src/org/robockets/buttonmanger/buttons/ActionButton.java
@@ -5,8 +5,16 @@ import org.robockets.buttonmanger.Button;
 import edu.wpi.first.wpilibj.command.Command;
 
 public class ActionButton extends Button {
+	private boolean killOnRelease = true;
 	
-	public ActionButton(int joystickNumber, int buttonNumber, Command command) {
+	public ActionButton(int joystickNumber, int buttonNumber, Command command, boolean killOnRelease) {
 		super(joystickNumber, buttonNumber, command);
+		this.killOnRelease = killOnRelease;
 	}
+	
+	protected void notPressed() {
+		command.cancel();
+	}
+	
+	
 }
