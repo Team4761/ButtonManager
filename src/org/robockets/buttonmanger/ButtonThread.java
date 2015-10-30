f70da45dce6cf68c049ea8ff898b83f4ae5677b1
Jake Kinsella <jake.kinsella@gmail.com>
2015-10-29 20:27:17 -0400

    Fix button thread timeouts

--- src/org/robockets/buttonmanger/ButtonThread.java
+++ src/org/robockets/buttonmanger/ButtonThread.java
@@ -1,23 +1,15 @@
 package org.robockets.buttonmanger;
 
 public class ButtonThread implements Runnable {
-	private long startTime = 0;
-
 	public void run() {
 		while (true) {
-			setTimeout();
 			ButtonManager.checkButtons();
-			waitUntilTimedOut();
-		}
-	}
-	
-	private void setTimeout() {
-		startTime = System.nanoTime();
-	}
-	
-	private void waitUntilTimedOut() {
-		while (System.nanoTime() >= startTime + 10000000) { // 10000000ns should be 10ms
-			// Just wait
+			
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 }
