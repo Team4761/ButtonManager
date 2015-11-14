06f410b79f5944612487d3cb9e3b56c1d0c9d474
Jake Kinsella <jake.kinsella@gmail.com>
2015-11-14 11:39:11 -0500

    Change enum to actual class because the seems way nicer

---
+++ src/org/robockets/buttonmanager/joysticks/Controller.java
@@ -0,0 +1,18 @@
+package org.robockets.buttonmanager.joysticks;
+
+public class Controller {
+	public static final int A = 1;
+	public static final int B = 1;
+	public static final int X = 1;
+	public static final int Y = 1;
+	public static final int RIGHT_BUMPER = 1;
+	public static final int LEFT_BUMPER = 1;
+	public static final int RIGHT_TRIGGER_AXIS = 1;
+	public static final int LEFT_TRIGGER_AXIS = 1;
+	public static final int LEFT_X_AXIS = 1;
+	public static final int LEFT_Y_AXIS = 1;
+	public static final int RIGHT_X_AXIS = 1;
+	public static final int RIGHT_Y_AXIS = 1;
+	
+	
+}
--- src/org/robockets/buttonmanager/joysticks/XboxOne.java
+++ src/org/robockets/buttonmanager/joysticks/XboxOne.java
@@ -1,25 +1,16 @@
 package org.robockets.buttonmanager.joysticks;
 
-public enum XboxOne {
-	A(1),
-	B(2),
-	X(3),
-	Y(4),
-	RIGHT_BUMPER(6),
-	LEFT_BUMPER(5),
-	RIGHT_TRIGGER_AXIS(3),
-	LEFT_TRIGGER_AXIS(2),
-	LEFT_X_AXIS(0),
-	LEFT_Y_AXIS(1),
-	RIGHT_X_AXIS(4),
-	RIGHT_Y_AXIS(5);
-	
-	private int buttonNumber = 0;
-	XboxOne(int buttonNumber) {
-        this.buttonNumber = buttonNumber;
-    }
-
-    public int getButtonNumber() {
-        return this.buttonNumber;
-    }
+final class XboxOne extends Controller {
+	public static final int A = 1;
+	public static final int B = 2;
+	public static final int X = 3;
+	public static final int Y = 4;
+	public static final int RIGHT_BUMPER = 6;
+	public static final int LEFT_BUMPER = 5;
+	public static final int RIGHT_TRIGGER_AXIS = 3;
+	public static final int LEFT_TRIGGER_AXIS = 2;
+	public static final int LEFT_X_AXIS = 0;
+	public static final int LEFT_Y_AXIS = 1;
+	public static final int RIGHT_X_AXIS = 4;
+	public static final int RIGHT_Y_AXIS = 5;
 }
